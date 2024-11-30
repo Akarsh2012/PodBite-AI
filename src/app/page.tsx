80d34bccdda0405322ddc08b8b298b6da4632c12
Akarsh Singh <akarshs641@gmail.com>
2024-11-30 15:29:10 +0530

    Added Landing Pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import Footer from "@/components/landing/Footer";
+import HeroSection from "@/components/landing/HeroSection";
+import Navbar from "@/components/landing/Navbar";
+import Pricing from "@/components/landing/Pricing";
+import React from "react";
+import { authOptions, type CustomSession } from "./api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 
-function page() {
+ async function page() {
+  const session: CustomSession| null = await getServerSession(authOptions);
   return (
-    <div>
-      I am App
-    </div>
-  )
+    <>
+      <Navbar  user={session?.user}/>
+      <HeroSection />
+      <Pricing user={session?.user} />
+      <Footer />
+    </>
+  );
 }
 
-export default page
+export default page;
